7822accd9ff82dcfb9ad147227b61f42fe94d3d6
Open Markets Initiative <contact@openmarketsinitiative.com>
2026-08-24 17:43:55 -0500

    Regenerate with different byte layout

--- .github/tests/test_cme_globex_mdp3_v1_12.py
+++ .github/tests/test_cme_globex_mdp3_v1_12.py
@@ -10,7 +10,8 @@ sys.path.insert(0, ".github/tests")
 import payloads
 
 SCHEMA = "cme/mdp3/Cme_Globex_Mdp3_v1_12.dfdl.xsd"
-PARSER = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "cme_globex_mdp3_v1_12.parser")
+PARSER_TCPPACKET = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "cme_globex_mdp3_v1_12_tcppacket.parser")
+PARSER_UDPPACKET = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "cme_globex_mdp3_v1_12_udppacket.parser")
 DAFFODIL = os.environ.get("DAFFODIL", "daffodil")
 
 
@@ -18,14 +19,15 @@ class CmeGlobexMdp3V112Tests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        subprocess.run([DAFFODIL, "save-parser", "-s", SCHEMA, "-r", "udpPacket", PARSER], check=True)
+        subprocess.run([DAFFODIL, "save-parser", "-s", SCHEMA, "-r", "tcpPacket", PARSER_TCPPACKET], check=True)
+        subprocess.run([DAFFODIL, "save-parser", "-s", SCHEMA, "-r", "udpPacket", PARSER_UDPPACKET], check=True)
 
     def test_mdincrementalrefreshbooklongqty(self):
         for payload in payloads.of("omi-data-packets/Cme/Mdp3.Sbe.v1.12/MdIncrementalRefreshBookLongQty.pcap"):
             data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
             with open(data, "wb") as handle:
                 handle.write(payload)
-            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER, data], capture_output=True)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_UDPPACKET, data], capture_output=True)
             self.assertEqual(result.returncode, 0, result.stderr.decode())
 
     def test_mdincrementalrefreshtradesummarylongqty(self):
@@ -33,7 +35,71 @@ class CmeGlobexMdp3V112Tests(unittest.TestCase):
             data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
             with open(data, "wb") as handle:
                 handle.write(payload)
-            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER, data], capture_output=True)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_UDPPACKET, data], capture_output=True)
+            self.assertEqual(result.returncode, 0, result.stderr.decode())
+
+    def test_marketdatarequest(self):
+        for payload in payloads.of("omi-data-packets/Cme/Mdp3.Sbe.v1.12/MarketDataRequest.Tcp.pcap"):
+            data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
+            with open(data, "wb") as handle:
+                handle.write(payload)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_TCPPACKET, data], capture_output=True)
+            self.assertEqual(result.returncode, 0, result.stderr.decode())
+
+    def test_requestack(self):
+        for payload in payloads.of("omi-data-packets/Cme/Mdp3.Sbe.v1.12/RequestAck.Tcp.pcap"):
+            data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
+            with open(data, "wb") as handle:
+                handle.write(payload)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_TCPPACKET, data], capture_output=True)
+            self.assertEqual(result.returncode, 0, result.stderr.decode())
+
+    def test_securitylistrequest(self):
+        for payload in payloads.of("omi-data-packets/Cme/Mdp3.Sbe.v1.12/SecurityListRequest.Tcp.pcap"):
+            data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
+            with open(data, "wb") as handle:
+                handle.write(payload)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_TCPPACKET, data], capture_output=True)
+            self.assertEqual(result.returncode, 0, result.stderr.decode())
+
+    def test_securitystatusrequest(self):
+        for payload in payloads.of("omi-data-packets/Cme/Mdp3.Sbe.v1.12/SecurityStatusRequest.Tcp.pcap"):
+            data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
+            with open(data, "wb") as handle:
+                handle.write(payload)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_TCPPACKET, data], capture_output=True)
+            self.assertEqual(result.returncode, 0, result.stderr.decode())
+
+    def test_securitystatus(self):
+        for payload in payloads.of("omi-data-packets/Cme/Mdp3.Sbe.v1.12/SecurityStatus.Tcp.pcap"):
+            data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
+            with open(data, "wb") as handle:
+                handle.write(payload)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_TCPPACKET, data], capture_output=True)
+            self.assertEqual(result.returncode, 0, result.stderr.decode())
+
+    def test_mdinstrumentdefinitionfx(self):
+        for payload in payloads.of("omi-data-packets/Cme/Mdp3.Sbe.v1.12/MdInstrumentDefinitionFx.Tcp.pcap"):
+            data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
+            with open(data, "wb") as handle:
+                handle.write(payload)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_TCPPACKET, data], capture_output=True)
+            self.assertEqual(result.returncode, 0, result.stderr.decode())
+
+    def test_snapshotfullrefreshtcplongqty(self):
+        for payload in payloads.of("omi-data-packets/Cme/Mdp3.Sbe.v1.12/SnapshotFullRefreshTcpLongQty.Tcp.pcap"):
+            data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
+            with open(data, "wb") as handle:
+                handle.write(payload)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_TCPPACKET, data], capture_output=True)
+            self.assertEqual(result.returncode, 0, result.stderr.decode())
+
+    def test_subscriberheartbeat(self):
+        for payload in payloads.of("omi-data-packets/Cme/Mdp3.Sbe.v1.12/SubscriberHeartbeat.Tcp.pcap"):
+            data = os.path.join(os.environ.get("RUNNER_TEMP", "/tmp"), "payload.bin")
+            with open(data, "wb") as handle:
+                handle.write(payload)
+            result = subprocess.run([DAFFODIL, "parse", "-P", PARSER_TCPPACKET, data], capture_output=True)
             self.assertEqual(result.returncode, 0, result.stderr.decode())
 
 
